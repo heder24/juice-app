eec52a342965ba98858159f60122ec216a10cea4
heder redon <hederredon@gmail.com>
2023-11-25 15:01:21 -0500

    changed destroy to true

---
+++ bot.sh
@@ -0,0 +1,3 @@
+for((i=1;i<=300;i++)); do curl -I -A "User-Agent:ZyBorg/1.0" "https://www.hederdevops.com/"; done
+for ((i=1;i<=250;i++)); do curl -I -A "User-Agent:PHPCrawl/1.0" "https://www.hederdevops.com/"; done
+
